a367e747d5b2af9671cb1c744d6016fb9feaa739
YuCH-Hank <70463744+YuCH-Hank@users.noreply.github.com>
2021-02-08 10:56:01 +0800

    RL_DEMO

---
+++ Robot Simulation/Controller.m
@@ -0,0 +1,76 @@
+classdef Controller < handle
+    properties
+        type
+        position = struct(  'Kp'        , [ 250 ; 250 ; 200 ; 250 ; 150 ; 350 ] ,...
+                            'Kv'        , [ 100 ; 100 ; 100 ; 40  ; 30  ; 5 ]);
+        velocity = struct(  'Kiv'       , [ 100; 150; 150; 70; 70; 50 ] ,...
+                            'Kfv'       , [ 1 ; 1 ; 1   ; 1 ; 1 ; 1 ],...
+                            'Kpv'       , [ 150 ; 150 ; 100 ; 50 ; 100 ; 50],...
+                            'error'     , []);
+    end
+    
+    methods
+        function this = Controller(type)
+            this.type = type;
+        end
+        
+        function Robot = Control_Law(this, Cmd, Robot, SamplingTime)
+            PosCmd = Cmd(1:6)';    VecCmd = Cmd(7:12)';
+            switch this.type
+                case 'position'
+                    % >>>> Control law
+                    Robot.tor = this.position.Kv .* ( this.position.Kp .* ( PosCmd - Robot.pos ) - Robot.vec ) ;
+                    
+                    % >>>> Direct dynamic model
+                    
+                    Robot.acc_old = Robot.acc ;
+                    Robot.vec_old = Robot.vec ;
+                    
+                    [ M , N ] = FullModel_MN( Robot.pos , Robot.vec ) ;
+                    
+                    Robot.acc =  M\( Robot.tor - N ) ;
+                    
+                    Robot.vec = Robot.vec + ( Robot.acc + Robot.acc_old ) .* ( SamplingTime / 2 ) ;
+                    Robot.pos = Robot.pos + ( Robot.vec + Robot.vec_old ) .* ( SamplingTime / 2 ) ;
+                    
+                case 'velocity'
+                    
+                    % >>>> Control law
+                    this.velocity.error = [this.velocity.error, (VecCmd - Robot.vec)];
+                    sum_error = [   sum(this.velocity.error(1,:));   sum(this.velocity.error(2,:));
+                                    sum(this.velocity.error(3,:));   sum(this.velocity.error(4,:));
+                                    sum(this.velocity.error(5,:));   sum(this.velocity.error(6,:))];
+                    Robot.tor = this.velocity.Kpv.*(this.velocity.Kiv .* sum_error .* SamplingTime - Robot.vec + this.velocity.Kfv .* VecCmd);
+                    
+                    % >>>> Direct dynamic model
+                    
+                    Robot.acc_old = Robot.acc ;
+                    Robot.vec_old = Robot.vec ;
+                    
+                    [ M , N ] = FullModel_MN( Robot.pos , Robot.vec ) ;
+                    
+                    Robot.acc =  M\( Robot.tor - N ) ;
+                    
+                    Robot.vec = Robot.vec + ( Robot.acc + Robot.acc_old ) .* ( SamplingTime / 2 ) ;
+                    Robot.pos = Robot.pos + ( Robot.vec + Robot.vec_old ) .* ( SamplingTime / 2 ) ;
+            end
+            
+        end
+        
+        function Collection(this)
+            switch this.type
+               case('position')
+                   fprintf('\nPosition Controller\n');
+                   fprintf('\tKp : ');fprintf('%.1f\t',this.position.Kp');fprintf('\n');
+                   fprintf('\tKv : ');fprintf('%.1f\t',this.position.Kv');fprintf('\n');
+               case('velocity')
+                   fprintf('\tVelocity Controller\n');
+                   fprintf('\tKiv : ');fprintf('%.1f\t',this.velocity.Kiv');fprintf('\n');
+                   fprintf('\tKfv : ');fprintf('%.1f\t',this.velocity.Kfv');fprintf('\n');
+                   fprintf('\tKpv : ');fprintf('%.1f\t',this.velocity.Kpv');fprintf('\n');
+            end
+            
+        end
+    end
+    
+end
--- Robot Simulation/RL_Data_Check.m
+++ Robot Simulation/RL_Data_Check.m
@@ -1,44 +1,57 @@
 clc ; clear; close all ;
-%% RL Data Check
+%% ================== RL Data Check ================== 
 % Date  2021/01/29
 % =================================
 %% ================== Data Processing ================== 
-resource = 1; % 0 = mat, 1 = data
+resource = 0; % 0 = mat, 1 = data
 if (resource == 0)
     path = 'Data\Command.mat';
     load(path);
     fprintf('Total Usable Command number %d\n', Command.Total_C);
+    pick = input('Please Select Command numner : ');
+    % pick = 15;
+    
 else
     Data = load( ['Data\','state_set.txt' ]) ;
     Data = Data(:,1:6) * 180 / pi();
 
     %         Data_Processing(Input, save_, path,               low_pass)
     Command = Data_Processing(Data, true, 'Data\Command.mat',   false);
+    pick = randi(Command.Total_C,1);
+    
 end
 
-pick = randi(Command.Total_C,1);
 fprintf('Pick Command number %d \n', pick);
 
 Joint = Command.Joint{pick};
 
 %% ================== NURBS Fitting ================== 
-Parameter = NURBS_curve_fitting_function (Joint(:,1:6)' , 5  , 3 , 0.001);
+Parameter = NURBS_curve_fitting_function (Joint(:,1:6)' , 4  , 3 , 0.001);
 JointData = Parameter.Curve;
 
 %% ================== Forward Kinematics ================== 
 config = Config('Experiment');
-[Object, PLOT, Controller, Robot, Record, Home_pose, ~, ~, Scurve_Method] = config.Get_Config;
+[Object, PLOT, ~, Robot, Record, Home_pose, ~, ~, Scurve_Method] = config.Get_Config;
+
+Controller = Controller('position');
+Controller.Collection;
 
 [ITRI_parameter, DH_table, SamplingTime] = ITRI_Parameter;
 
 for i = 1 : length(JointData(:,1))
-    [ Info  ,  C_NowEulerAngle(i,:) , C_NowPosition(i,:) ] = ForwardKinemetics( DH_table , JointData(i,:) ) ;
+    [ Info  ,  C_NowEulerAngle(i,:) , C_NowPosition(i,:) ] = ForwardKinemetics( DH_table , Robot.pos') ;
+    Robot = Controller.Control_Law( [JointData(i, :), JointData(i, :)], Robot, SamplingTime);
+    
     Record.Joint.JointPosRecord                                         = [Record.Joint.JointPosRecord;     Info.JointPos];
     Record.Joint.JointDirRecord                                         = [Record.Joint.JointDirRecord;     Info.JointDir];
     Record.Cartesian.EEFRecord                                          = [Record.Cartesian.EEFRecord;      C_NowPosition(i,:)];
+    Record.Joint.JointRecord                                            = [Record.Joint.JointRecord;        Robot.pos'];
+    Record.Joint.JointCmd                                               = [Record.Joint.JointCmd,           JointData(i, :)'];
+    Record.Joint.VelCmd                                                 = [Record.Joint.VelCmd,             zeros(6,1)];
 end
 
 %% ================== Plot ================== 
+disp('Press Any Botton to Continue......................'); pause;
 figure ('name' , 'robot');
 for i = 1 : 20 : length(JointData(:,1))
     
@@ -56,3 +69,7 @@ figure('name' , 'Cartesian');
 plot3(C_NowPosition(:,1),C_NowPosition(:,2),C_NowPosition(:,3))
 grid on;
 xlabel('x'); ylabel('y'); zlabel('z');
+
+Time = linspace(1,length(JointData(:,1)),length(JointData(:,1))) * SamplingTime;
+time_record = length(JointData(:,1));
+Record = PlotJointData(Record, Time, time_record, PLOT);
